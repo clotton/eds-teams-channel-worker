ba81515f810d80555362337aac2141a8fd69cf1e
clotton <clotton@adobe.com>
2025-04-30 15:35:57 -0400

    commit

--- src/main.js
+++ src/main.js
@@ -57,8 +57,8 @@ function parseCsvToJson(csvString) {
 }
 
 // Helper: fetch all members for a team and count members/guests
-async function getTeamMemberCounts(accessToken, teamId) {
-  const url = `https://graph.microsoft.com/v1.0/teams/${teamId}/members`;
+async function getTeamSummary(accessToken, teamId) {
+  const url = `https://graph.microsoft.com/v1.0/teams/${teamId}`;
 
   const response = await fetch(url, {
     method: 'GET',
@@ -75,7 +75,7 @@ async function getTeamMemberCounts(accessToken, teamId) {
 
   const data = await response.json();
 
-  return  data.value.length ;
+  return  data ;
 }
 
 // Main function: fetch, filter, and format Teams activity report
@@ -120,15 +120,24 @@ async function getTeamActivityReport(accessToken, nameFilter = '', descriptionFi
   // Parallel fetch member counts for all teams
   const results = await Promise.all(filtered.map(async team => {
     const teamId = team['Team Id'];
-    const memberCountResponse = await getTeamMemberCounts(accessToken, teamId);
+    const teamSummary = await getTeamSummary(accessToken, teamId);
+
+    if (teamSummary === -1) {
+      return null; // Skip this team if there's an error fetching the summary
+    }
+
+    const totalMembers = teamSummary.summary.guestsCount + teamSummary.summary.membersCount;
 
     return {
       teamId: teamId,
-      teamName: team['Team Name'] || team['Team Display Name'],
+      teamName: team['Team Name'] || '',
+      description: teamSummary.description || '',
+      created: teamSummary.createdDateTime,
       lastActivityDate: team['Last Activity Date'],
-      channelMessages: team['Channel Messages'] || team['Number of Channel Messages'],
+      messageCount: team['Channel Messages'],
       activeChannels: team['Active Channels'],
-      memberCount: memberCountResponse
+      memberCount: totalMembers,
+      webUrl: teamSummary.webUrl || '',
     };
   }));
 
